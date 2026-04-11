b49fc2afff220429f1fa669fbb3abf7d0a55ef01
Wilson Jimenez <wijimenezz@gmailcom>
2026-04-11 13:43:16 -0500

    feats: se agregan: capertas para controladores y rutas, archivo .env.example, se agregar script npm start para iniciar proyecto y se levanta servidor usando dotenv para importar puerto

--- src/server.js
+++ src/server.js
@@ -1,2 +1,11 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT ||3000;
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+})
