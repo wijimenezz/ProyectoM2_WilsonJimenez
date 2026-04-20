f01f43ecae228e3991bbd336cd1a8bfb2fee8e0c
Wilson Jimenez <wijimenezz@gmailcom>
2026-04-20 11:26:28 -0500

    feats: instalo swagger-ui-express, yamljs para ver la documentacion en localhost3000/api-docs

--- src/app.js
+++ src/app.js
@@ -3,12 +3,17 @@ import express from "express";
 import routes from "./routes/index.js";
 import { errorHandler } from "./Middlewares/globalMiddleware.js";
 import cors from "cors";
+import YAML from "yamljs";
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/api", routes);
-app.use(errorHandler);
 
+const swaggerDocument = YAML.load("./openapi.yaml");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use(errorHandler);
 export default app;
