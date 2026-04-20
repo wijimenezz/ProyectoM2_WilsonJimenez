8a09b2ceca3e0c57bb454637366d57348ce20c3b
Wilson Jimenez <wijimenezz@gmailcom>
2026-04-20 10:49:36 -0500

    importo cors and app.js para probar rutas en Swagger UI

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import "dotenv/config";
 import express from "express";
 import routes from "./routes/index.js";
 import { errorHandler } from "./Middlewares/globalMiddleware.js";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", routes);
 app.use(errorHandler);
